1d20f3794e86ab222838755a4b146de7535ec341
Poojitha Vishwanadula <133566362+poojihere@users.noreply.github.com>
2023-07-09 15:28:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-searchForm = documnet.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
-
-
-
-window.onscroll = () =>{
-
-    searchForm.classList.remove('active');
-
-    if(window.scrollY>80){
-        document.querySelector('header .header-2').classList.add('active');
-    }
-    else{
-        document.querySelector('header .header-2').classList.remove('active');
-    }
-}
-
-window.onload = () =>{
-    if(window.scrollY>80){
-        document.querySelector('header .header-2').classList.add('active');
-    }
-    else{
-        document.querySelector('header .header-2').classList.remove('active');
-    }
-}
 searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
@@ -78,5 +50,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-
-});
