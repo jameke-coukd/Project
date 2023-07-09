09d07542e6433990d991f5976130fecd6a8ec1e6
Poojitha Vishwanadula <133566362+poojihere@users.noreply.github.com>
2023-07-09 16:44:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ window.onload = () =>{
     }
     //fadeOut();
 }
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     var swiper = new Swiper(".books-slider .swiper-container", {
       // Configure the options for the Swiper carousel
       slidesPerView: "auto",
@@ -48,5 +48,53 @@ document.addEventListener("DOMContentLoaded", function () {
         prevEl: ".books-slider .swiper-button-prev",
       },
     });
-  });
-
+  });*/
+ 
+  var swiper = new Swiper('.featured-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000, // Adjust the delay (in milliseconds) between slides
+        disableOnInteraction: false, // Allow user interaction to interrupt autoplay
+    },
+});
+var swiper = new Swiper('.arrivals-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 4000, // Adjust the delay (in milliseconds) between slides
+        disableOnInteraction: false, // Allow user interaction to interrupt autoplay
+    },
+});
+var swiper = new Swiper('.reviews-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000, // Adjust the delay (in milliseconds) between slides
+        disableOnInteraction: false, // Allow user interaction to interrupt autoplay
+    },
+});
+var swiper = new Swiper('.blogs-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000, // Adjust the delay (in milliseconds) between slides
+        disableOnInteraction: false, // Allow user interaction to interrupt autoplay
+    },
+});
